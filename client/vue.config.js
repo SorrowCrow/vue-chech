558d50e3043dcdd0dd07ae93be46e4c94fbaf96b
SorrowCrow <78910309+SorrowCrow@users.noreply.github.com>
2021-07-30 04:45:28 +0300

    critical css

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 const path = require("path");
-const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+// const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 module.exports = {
     configureWebpack: {
-        plugins: [
-            // new HtmlCriticalWebpackPlugin({
-            //     base: path.join(path.resolve(__dirname), "dist/"),
-            //     src: "index.html",
-            //     inline: true,
-            //     extract: true,
-            //     dest: "index.html",
-            //     width: 1625,
-            //     height: 773,
+        // plugins: [
+        //     new HtmlCriticalWebpackPlugin({
+        //         base: path.join(path.resolve(__dirname), "dist/"),
+        //         src: "index.html",
+        //         inline: true,
+        //         extract: true,
+        //         dest: "index.html",
+        //         width: 1625,
+        //         height: 773,
 
-            //     /* Ensure that bundled JS file is called */
-            //     penthouse: {
-            //         blockJSRequests: false
-            //     }
-            // })
-        ],
+        //         /* Ensure that bundled JS file is called */
+        //         penthouse: {
+        //             blockJSRequests: false
+        //         }
+        //     })
+        // ],
         devServer: {
             https: {
                 key: fs.readFileSync("../server.key"),
